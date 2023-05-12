9440e4d915f5f149427e8ebb1a81b2200c6b2e40
EmTran0 <emtran.0401@gmail.com>
2023-05-12 12:33:36 -0500

    character information
    This file contains character gift preferences, interests, marriage eligibility, and other information.

---
+++ npc_data.py
@@ -0,0 +1,52 @@
+# npc information
+# all character images were obtained from sticknpg.com
+
+characters = {'Leah': {'gifts': ['Loves: salad, wine', 'Likes: honey, cheese', 'Hates: bread, pizza'],
+                       'interests': ['Pottery and nature'],
+                       'birthday': ['BD: Winter 23'],
+                       'marriage': ['Marriage candidate? Yes'],
+                       'extra': ['Images/leah.png', '#B4E0A4']
+                       },
+              'Haley': {'gifts': ['Loves: coconuts', 'Likes: daffodils', 'Hates: clay, snails'],
+                        'interests': ['Money and fashion'],
+                        'birthday': ['BD: Spring 13'],
+                        'marriage': ['Marriage candidate? Yes'],
+                        'extra': ['Images/haley.png', '#EDB0E3']
+                        },
+              'Pam': {'gifts': ['Loves: beer, mead', 'Likes: daffodils, fruit', 'Hates: holly, squid'],
+                      'interests': ['Alcohol and driving'],
+                      'birthday': ['BD: Spring 18'],
+                      'marriage': ['Marriage candidate? No'],
+                      'extra': ['Images/pam.png', '#CBACF1']
+                      },
+              'Evelyn': {'gifts': ['Loves: tulips, beets', 'Likes: daffodils, milk', 'Hates: clams, sushi'],
+                         'interests': ['Baking and gardening'],
+                         'birthday': ['BD: Winter 20'],
+                         'marriage': ['Marriage candidate? No'],
+                         'extra': ['Images/evelyn.png', '#F1A49E']
+                         },
+              'Elliott': {'gifts': ['Loves: squid ink', 'Likes: gems, squid', 'Hates: quartz, amaranth'],
+                          'interests': ['Writing and movies'],
+                          'birthday': ['BD: Fall 5'],
+                          'marriage': ['Marriage candidate? Yes'],
+                          'extra': ['Images/elliot.png', '#9EF1C0']
+                          },
+              'Sebastian': {'gifts': ['Loves: sashimi', 'Likes: flounder, quartz', 'Hates: clay, omelets'],
+                            'interests': ['Programming and music'],
+                            'birthday': ['BD: Winter 10'],
+                            'marriage': ['Marriage candidate? Yes'],
+                            'extra': ['Images/sebastian.png', '#A5A5A5']
+                            },
+              'Harvey': {'gifts': ['Loves: coffee, wine', 'Likes: daffodils, leeks', 'Hates: coral, trash'],
+                         'interests': ['Medicine and nature'],
+                         'birthday': ['BD: Winter 14'],
+                         'marriage': ['Marriage candidate? Yes'],
+                         'extra': ['Images/harvey.png', '#CEA688']
+                         },
+              'Lewis': {'gifts': ['Loves: green tea', 'Likes: coconuts', 'Hates: holly, quartz'],
+                        'interests': ['Being mayor and festivals'],
+                        'birthday': ['BD: Spring 7'],
+                        'marriage': ['Marriage candidate? No'],
+                        'extra': ['Images/lewis.png', '#EADD8E']
+                        }
+              }
